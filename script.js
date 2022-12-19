0af60d248277912fc2505fe88670e8efa1ef7fa3
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2022-12-19 20:04:45 +0100

    final

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let base =  10;
-let height = 13;
-console.log((base*height)/2);
+//let base =  10;
+//let height = 13;
+//console.log((base*height)/2);
 
+function Area() {
+    var base = parseInt(document
+        .getElementById("base").value);
+     console.log (base);
+    var height = parseInt(document
+        .getElementById("height").value);
+
+    console.log(typeof(base));
+    var Area = (base + height) / 2;
+
+    //var area = Math.sqrt(s * ((s - side1) 
+      //              * (s - side2) * (s - side3)));
+    document.getElementById(
+        "display").innerHTML = area;
+}
